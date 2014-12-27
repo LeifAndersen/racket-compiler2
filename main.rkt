597a399871dc84180fca533e4d69a07b07c8c7ab
Leif Andersen <leif@leifandersen.net>
2014-12-27 12:35:00 -0700

    Parse into structs

--- main.rkt
+++ main.rkt
@@ -1,35 +1,138 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require racket/match
+         racket/pretty
+         syntax/parse
+         compiler/zo-marshal
+         (prefix-in zo: compiler/zo-structs)
+         "structs.rkt")
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco doc <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(define cc (current-compile))
 
-;; Code here
+(define (parse-top form)
+  (syntax-parse form
+    #:literals (#%expression module #%plain-module-begin begin begin-for-syntax)
+    [(#%expression body)
+     (topexpr (parse-expr #'body))]
+    [(module id:id path
+       (#%plain-module-begin body ...))
+     (topmod #'id (syntax->datum #'path)
+             (for/list ([i (syntax->list #'(body ...))])
+               (parse-mod i)))]
+    [(begin body ...)
+     (topbeg (for/list ([i (syntax->list #'(body ...))])
+               (parse-top i)))]
+    [(begin-for-syntax body ...)
+     (topbegfs (for/list ([i (syntax->list #'(body ...))])
+                 (parse-top i)))]
+    [else
+     (topgenform (parse-gen #'else))]))
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+(define (parse-mod form)
+  (syntax-parse form
+    #:literals (#%provide begin-for-syntax #%declare module module*
+                          #%plain-module-begin)
+    [(#%provide spec ...)
+     (pro (syntax->list #'(spec ...)))]
+    [(begin-for-syntax body ...)
+     (modbegfs (for/list ([i (syntax->list #'(body ...))])
+                 (parse-mod i)))]
+    [(#%declare keyword ...)
+     (decl (syntax->list #'(keyword ...)))]
+    [(module id:id path
+       (#%plain-module-begin body ...))
+     (submod #'id (syntax->datum #'path)
+             (for/list ([i (syntax->list #'(body ...))])
+               (parse-mod i)))]
+    [(module* id:id path
+       (#%plain-module-begin body ...))
+     (submod* #'id (syntax->datum #'path)
+              (for/list ([i (syntax->list #'(body ...))])
+                (parse-mod i)))]
+    [else
+     (modgenform (parse-gen #'else))]))
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+(define (parse-gen form)
+  (syntax-parse form
+    #:literals (define-values define-syntaxes #%require)
+    [(define-values (id:id ...) body)
+     (defv (syntax->list #'(id ...)) (parse-expr #'body))]
+    [(define-syntaxes (id:id ...) body)
+     (defsyntaxs (syntax->list #'(id ...)) (parse-expr #'body))]
+    [(#%require spec ...)
+     (req (syntax->list #'(spec ...)))]
+    [else
+     (parse-expr #'else)]))
+
+(define (parse-expr form)
+  (syntax-parse form
+    #:literals (#%plain-lambda case-lambda if begin begin0 let-values
+                letrec-values set! quote quote-syntax with-continuation-mark
+                #%plain-app #%top #%variable-reference)
+    [id:id (ide #'id)]
+    [(#%plain-lambda formals body ...+)
+     (parse-lambda #'formals #'(body ...))]
+    [(case-lambda (formals body ...+) ...)
+     (caselam
+      (for/list ([formal (syntax->list #'(formals ...))]
+                 [b (syntax->list #'((body ...) ...))])
+        (parse-lambda formal b)))]
+    [(if test tbranch fbranch)
+     (branch (parse-expr #'test) (parse-expr #'tbranch) (parse-expr #'fbranch))]
+    [(begin body ...+)
+     (beg (for/list ([b (syntax->list #'(body ...))])
+            (parse-expr b)))]
+    [(begin0 body ...+)
+     (beg0 (for/list ([b (syntax->list #'(body ...))])
+             (parse-expr b)))]
+    [(let-values ([(ids:id ...) val] ...)
+       body ...+)
+     (letv (for/list ([i (syntax->list #'((ids ...) ...))]
+                      [v (syntax->list #'(val ...))])
+             (defv (syntax->list i) (parse-expr v)))
+           (for/list ([b (syntax->list #'(body ...))])
+             (parse-expr b)))]
+    [(letrec-values ([(ids:id ...) val] ...)
+       body ...+)
+     (letrecv (for/list ([i (syntax->list #'((ids ...) ...))]
+                         [v (syntax->list #'(val ...))])
+                (defv (syntax->list i) (parse-expr v)))
+              (for/list ([b (syntax->list #'(body ...))])
+                (parse-expr b)))]
+    [(set! id:id body)
+     (setv #'id (parse-expr #'body))]
+    [(quote datum)
+     (quo (syntax->datum #'datum))]
+    [(with-continuation-mark key val result)
+     (withcm (parse-expr #'key) (parse-expr #'val) (parse-expr #'result))]
+    [(#%plain-app func body ...)
+     (app (parse-expr #'func)
+          (for/list ([i (syntax->list #'(body ...))])
+            (parse-expr i)))]
+    [(#%top . id:id)
+     (top #'id)]
+    [(#%variable-reference . id:id)
+     (varref #'id:id)]
+    [(#%variable-reference (#%top . id:id))
+     (varref (top #'id))]
+    [(#%variable-reference)
+     (varref #f)]))
+
+(define (parse-lambda formals body)
+  (define body* (for/list ([i (syntax->list body)])
+                  (parse-expr i)))
+  (syntax-parse formals
+    [(ids:id ...)
+     (lam (syntax->list #'(ids ...)) #f body*)]
+    [(ids:id ... . rest:id)
+     (lam (syntax->list #'(ids ...)) #'rest body*)]
+    [rest:id
+     (lam null #'rest body*)]))
+
+(define (new-cc form y)
+  (define expanded-form (expand form))
+  (define parsed (parse-top expanded-form))
+  (pretty-write form)
+  (cc form y))
+
+(current-compile new-cc)
--- structs.rkt
+++ structs.rkt
@@ -7,7 +7,7 @@
 (struct topgenform topform ([body : genform])
         #:transparent)
 
-(struct topexp topform ([body : expr])
+(struct topexpr topform ([body : expression])
         #:transparent)
 
 (struct topmod topform ([id : Identifier]
@@ -29,85 +29,85 @@
 (struct pro modform ([spec : (Listof Any)]) ;; TODO
         #:transparent)
 
-(struct begfs modform ([body : (Listof modform)])
+(struct modbegfs modform ([body : (Listof modform)])
         #:transparent)
 
 (struct decl modform ([body : (Listof Any)]) ;; TODO
         #:transparent)
 
-(struct submod modform () #:transparent)
+(struct submodform modform () #:transparent)
 
-(struct mod submod ([id : Identifier]
+(struct submod submodform ([id : Identifier]
                     [path : Module-Path]
-                    [body : modform])
+                    [body : (Listof modform)])
         #:transparent)
 
-(struct mod* submod ([id : Identifier]
+(struct submod* submodform ([id : Identifier]
                      [path : (U Module-Path #f)]
-                     [body : modform])
+                     [body : (Listof modform)])
         #:transparent)
 
 (struct genform () #:transparent)
 
-(struct defvals genform ([id : (Listof Identifier)]
-                 [body : expr])
+(struct defv genform ([id : (Listof Identifier)]
+                      [body : expression])
         #:transparent)
 
-(struct defsyntaxs genform ([id : (Listof Identifier)]
-                    [body : expr])
-        #:transparent)
+(struct defsyntaxs defv () #:transparent)
 
 (struct req genform ([spec : (Listof Any)]) ;; TODO
         #:transparent)
 
-(struct expr genform () #:transparent)
+(struct expression genform () #:transparent)
 
-(struct lam expr ([args : (Listof Identifier)]
-                  [rest : (U Identifier #f)]
-                  [body : expr])
+(struct ide expression ([id : Identifier])
         #:transparent)
 
-(struct caselam expr ([lams : (Listof lam)])
+(struct lam expression ([args : (Listof Identifier)]
+                        [rest : (U Identifier #f)]
+                        [body : (Listof expression)])
         #:transparent)
 
-(struct branch expr ([condition : expr]
-                     [consequent : expr]
-                     [alternative : expr])
+(struct caselam expression ([lams : (Listof lam)])
         #:transparent)
 
-(struct beg expr ([body : (Listof expr)])
+(struct branch expression ([test : expression]
+                           [tbranch : expression]
+                           [fbranch : expression])
         #:transparent)
 
-(struct beg0 expr ([body : (Listof expr)])
+(struct beg expression ([body : (Listof expression)])
         #:transparent)
 
-(struct letv expr ([id : (Listof Identifier)]
-                   [val : expr]
-                   [body : expr])
+(struct beg0 expression ([body : (Listof expression)])
+        #:transparent)
+
+(struct letv expression ([defs : (Listof defv)]
+                         [body : expression])
         #:transparent)
 
 (struct letrecv letv () #:transparent)
 
-(struct setv expr ([id : Identifier]
-                   [body : expr])
+(struct setv expression ([id : Identifier]
+                         [body : expression])
         #:transparent)
 
-(struct quo expr ([datum : Any])
+(struct quo expression ([datum : Any])
         #:transparent)
 
-(struct quosyntax expr ([datum : Any])
+(struct quosyntax expression ([datum : Any])
         #:transparent)
 
-(struct withcm expr ([key : expr]
-                     [val : expr]
-                     [result : expr])
+(struct withcm expression ([key : expression]
+                           [val : expression]
+                           [result : expression])
         #:transparent)
 
-(struct app expr ([f : expr]
-                  [args : (Listof expr)])
+(struct app expression ([f : expression]
+                        [args : (Listof expression)])
         #:transparent)
 
-(struct top expr ([id : Identifier])
+(struct top expression ([id : Identifier])
         #:transparent)
 
 (struct varref ([id : (U top Identifier #f)])
